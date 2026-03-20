d8a62f2af5cf34ac019b4324cf6f50f29925cb77
coral <coral.persimmon@gmail.com>
2026-03-20 10:42:47 +0800

    商品功能-Refactor category，使用Enum類型

--- src/main/java/com/coralpersimmon/mymall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/coralpersimmon/mymall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.coralpersimmon.mymall.rowmapper;
 
+import com.coralpersimmon.mymall.constant.ProductCategory;
 import com.coralpersimmon.mymall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,12 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+        /*
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+        */
+        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
